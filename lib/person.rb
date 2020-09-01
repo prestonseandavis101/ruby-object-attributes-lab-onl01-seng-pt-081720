b60a2763d7e0aac87703035e2b50a4b207992291
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-01 01:45:46 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+end
+
+def name
+  @name
+end
+end
+
+beyonce = Person.new 
+beyonce.name = "Beyonce"
+puts beyonce.name
+
+class Person
+  def job=(person_job)
+    @job = person_job
+  end
+  
+  def job
+    @job
+  end
+end
+
+singer = Person.new
+singer.job = "Singer"
+puts singer.job
